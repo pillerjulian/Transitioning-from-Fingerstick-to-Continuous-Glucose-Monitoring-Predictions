ad4ee9a62bfd7535ef521aceebc84319e2c49d4d
pillerjulian <julian.piller@outlook.com>
2023-11-11 17:26:10 +0100

    Tested on ubelix

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-#SBATCH --mail-user=diego.zeiter1@students.unibe.ch
-#SBATCH --mail-type=start, end, fail
-#SBATCH --time=24:00:00 
-#SBATCH --mem-per-cpu=1G
-#SBATCH --cpus-per-task=1 
-#SBATCH --job-name="Diabetes Project"
 module load Anaconda3 ## load available module, show modules -> module avail
 eval "$(conda shell.bash hook)" ## init anaconda in shell
 conda activate diabetesProject ## activate environment
