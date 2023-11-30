6e135071c9bd1443f5cd55c6b86edf5ad80df9b1
Daniel Kerber <103103280+kerberdaniel@users.noreply.github.com>
2023-11-30 11:41:54 +0100

    Last changes with correct data loader and comments on each function

--- src/main.py
+++ src/main.py
@@ -23,7 +23,9 @@ warnings.filterwarnings("ignore")
 
 def add_artificial_SMBG(data, time_between_two_SMBGs_in_h):
     """
-    Add artificial SMBG values to the data
+    Add artificial SMBG values to the data so we assure we have at least a few SMBG value in the prediction window
+    as some patients had sparse finger smbg tests. In the report, we tested every 2h, 4h, 8h artificial smbg added.
+
     :param data: data to add artificial SMBG values to
     :param time_between_two_SMBGs_in_h: maximal time window between two SMBG values
     :return: data with artificial SMBG values and the indices of the artificial SMBG values
@@ -72,7 +74,14 @@ def add_artificial_SMBG(data, time_between_two_SMBGs_in_h):
 
 def preprocess_data(data, SMBG_window):
     """
-    Preprocess the data
+    Preprocess the data - Given that there are some missing cbg values, we interpolate them only if the gap (size of
+    consecutive missing values)  is not too large (greater than 12h without cbg). If the gap is too large,
+    we drop the whole window and concatenate the data to avoid bug on the interpolation.
+
+    We also replace SMBG values where the absolute difference exceeds the threshold and SMBG is not NaN. We also
+    replace any 0 values in 'finger' with corresponding values from 'cbg'. Finally, we add artificial SMBG values to
+    increase the frequency of the data. Additionally, we fill NaN values in 'bolus' and 'carbInput' with 0.
+
     :param data: data to preprocess
     :param SMBG_window: SMBG window
     :return: preprocessed data and the indices of the artificial SMBG values
@@ -128,7 +137,20 @@ def preprocess_data(data, SMBG_window):
 
 def read_patient(train_set, test_set, prediction_window, SMBG_window):
     """
-    Read the data of a patient and preprocess it
+    Read the data of a patient and call the preprocess functions onto the train and test set. We create the arrays
+    containing the features, were X has the explanatory variables ['finger', 'basal', 'carbInput', 'bolus'] and Y has
+    the target variable ['cbg']. We also scale the data from 0 to 1 for each individual feature so we can train the
+    model. Finally, we split the data into windows so that the model can train as expected.
+
+    X_train = [window1, window2, ..., windowN] where window1 = [finger1, basal1, carbInput1, bolus1]
+    Y_train = [window1, window2, ..., windowN] where window1 = [cbg1]
+
+    X_train and Y_train are overlapping windows of size prediction_window. This overlapping was chosen to obtain more
+    training windows for the model training.
+
+    For the test set, X_test and Y_test are not overlapping windows of size prediction_window. This is because we want
+    to predict the entire window of Y_test with the correspondent window of X_test.
+
     :param train_set: training set
     :param test_set: testing set
     :param prediction_window: prediction window
@@ -192,7 +214,7 @@ def read_patient(train_set, test_set, prediction_window, SMBG_window):
         scaled_column = scaler.fit_transform(column.reshape(-1, 1))
 
         # We store a -1 value where there is a NaN value
-        scaled_column[isnan_mask] = 0  # we can set here either to -1 or 0, still have # TODO
+        scaled_column[isnan_mask] = 0
 
         scaled_test_set[:, i] = scaled_column.flatten()
 
@@ -203,14 +225,7 @@ def read_patient(train_set, test_set, prediction_window, SMBG_window):
     x_train = []  # holds the features (all except cbg) with window size equal the prediction window
     y_train = []  # holds the cbg with window size equal the prediction window
 
-    # Append the features into x_train and cbg into y_train, considering prediction_window for each
-    # i = prediction_window
-    # while i < len(scaled_train_set):
-    #     x_train.append(scaled_train_set[i - prediction_window:i, 1:])
-    #     y_train.append(scaled_train_set[i - prediction_window:i, 0])
-    #     i = i + prediction_window
-
-    # # Splitting the vector where y_train is cbg and x_train has the other features
+    # Splitting the vector where y_train is cbg and x_train has the other features
     for i in range(prediction_window, len(scaled_train_set)):
         x_train.append(scaled_train_set[i - prediction_window:i, 1:])
         y_train.append(scaled_train_set[i - prediction_window:i,
@@ -254,26 +269,43 @@ def train_model(x_train, y_train, batch_size, epochs, learning_rate):
     :param epochs: number of epochs
     :param learning_rate: learning rate
     :return: the trained model and the history of the training
+
+    The values printed below are only an example of the shapes of the data. Values of number of windows change
+    depending on the patient being tested.
+
+    TRAINING: To have significantly more trainings windows, we are moving the prediction window always 1 to the
+    right, giving a total of 11531 windows, the prediction window size is 80 and the number of features is 4 (
+    finger, basal, carbInput, bolus)
+    print(x_train.shape) (11531, 80, 4)
+    print(y_train.shape) (11531, 80)
+
+    TESTING: Here we don't want to move the prediction window 1 to the right, we want to predict the entire window
+    of y_test with the correspondent window of x_test. So we have 35 windows of prediction window size is 80 and
+    the number of features is 4 (finger, basal, carbInput, bolus).
+    print(x_test.shape) (35, 80, 4)
+    print(np.array(y_test).shape) (35, 80)
+
     """
+
     prediction_window_size = x_train.shape[1]
     num_features = x_train.shape[2]  # number of features except cbg (finger, basal, carbInput, bolus)
 
     model = Sequential()
     model.add(
         LSTM(units=100, return_sequences=True, activation="tanh", input_shape=(prediction_window_size, num_features)))
-    model.add(BatchNormalization())  # Added Batch Normalization layer
-    model.add(LSTM(units=100, return_sequences=False, activation="tanh"))
-    # model.add(BatchNormalization())  # Added Batch Normalization layer
-    model.add(Dropout(0.2))  # TODO: Try more/less dropout and or remove it
+    model.add(BatchNormalization())
+    model.add(LSTM(units=100, return_sequences=False, activation="tanh"))  # We tried also Relu, but it made the
+    # prediction worse
+    # model.add(BatchNormalization()) # Adding batch norm here, also made prediction worse here
+    model.add(Dropout(0.2))
+
+    # Here the shape of Dense is 80 because we want to predict 80 values of cbg per X_train window
     model.add(Dense(units=y_train.shape[1]))
 
     custom_optimizer = Adam(learning_rate=learning_rate)
 
     # model.compile(optimizer="adam", loss='mse')
     model.compile(optimizer=custom_optimizer, loss='mse')
-
-    # TODO: try with Relu activation function
-    # TODO: try more LSTM Units     # the original is 50 50
 
     history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs)
 
@@ -294,19 +326,18 @@ def make_prediction(scalers_transforms_test, model, x_test, y_test):
     predictions = np.reshape(predictions, (predictions.shape[0], predictions.shape[1]))  # reshape to match y_test
     predictions = scalers_transforms_test[0].inverse_transform(predictions)  # bring values back to original scale
 
-    # creates a 1D prediction vector of cgm, used for plotting only
+    # Concatenate the predictions windows of size prediction_window to a single vector for plotting
     predicted_cbg = predictions[0]
-    for i in range(1, len(predictions)):  # TODO: Check if its correct
+    for i in range(1, len(predictions)):
         predicted_cbg = np.concatenate([predicted_cbg, predictions[i]])
 
-    # print("shape predicted_cbg:", predicted_cbg.shape) # shape predicted_cbg: (2800,)
+    # Example shape predicted_cbg: (2800,) --> 35 windows of 80 values each
+
+    y_test = np.array(y_test)
+    # print("shape y_test:", y_test.shape)  # shape y_test: (35, 80)
+    # print("shape prediction:", predictions.shape)  # shape prediction: (35, 80)
 
     # Calculate the RMSE
-    y_test = np.array(y_test)
-
-    print("shape y_test:", y_test.shape)  # shape y_test: (35, 80)
-    print("shape prediction:", predictions.shape)  # shape prediction: (35, 80)
-
     rmse = round(np.sqrt(np.mean(((predictions - y_test) ** 2))), 4)
 
     return predictions, predicted_cbg, rmse
@@ -316,6 +347,8 @@ def create_and_save_plots(continuous_ytest, continuous_predictions, test_set_plo
                           batch_size, epochs, learning_rate, prediction_window,
                           test_artificial_SMBG_indices, inter_statement, SMBG_window, patient_index, data_from_2018):
     '''
+    Create and save plots for the test set with the predicted and ground truth CBG lines, containing also the real
+    and artificial SMBG values and the plot for the training loss
 
     :param continuous_ytest:
     :param continuous_predictions:
@@ -330,18 +363,25 @@ def create_and_save_plots(continuous_ytest, continuous_predictions, test_set_plo
     :param inter_statement:
     :return:
     '''
+
+    patient_ids_2018 = [559, 563, 570, 575, 588, 591]
+    patient_ids_2020 = [540, 544, 552, 567, 584, 596]
+
+    if data_from_2018:
+        plotted_patient_id = patient_ids_2018[patient_index]
+    else:
+        plotted_patient_id = patient_ids_2020[patient_index]
+
     # Create a directory for saving plots inside the 'results' folder
     time_stamp = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
     dir_name = f"results/plots_{time_stamp}"  # Prepend 'results/' to the directory name
     os.makedirs(dir_name, exist_ok=True)
     # Plotting the predictions
     plt.figure(figsize=(16, 8))
-    plt.title(f'Blood Glucose Prediction Model Result with RMSE: {rmse} ({inter_statement}) (ID: {patient_index}) (is '
-              f'2018?{data_from_2018})')
+    plt.title(f'Blood Glucose Prediction Model Result with RMSE: {rmse} ({inter_statement}) '
+              f'(Patient ID: {plotted_patient_id})')
     plt.plot(continuous_ytest, color='b')
-    # plt.plot(np.arange(len(test_set_plotting['cbg'].iloc[:len(continuous_predictions)])), test_set_plotting['cbg'].iloc[:len(continuous_predictions)],color='blue')
     plt.plot(continuous_predictions, color='r')
-    # plt.scatter(np.arange(len(smbg_scatter)), smbg_scatter, color='black', marker='o')
 
     plt.plot(np.arange(len(test_set_plotting['finger'].iloc[:len(continuous_predictions)])),
              test_set_plotting['finger'].iloc[:len(continuous_predictions)], marker='o', linestyle='-',
@@ -354,7 +394,7 @@ def create_and_save_plots(continuous_ytest, continuous_predictions, test_set_plo
                 zorder=5)  # plot the artifical SMBG values in another color
     plt.xlabel('Timestamp', fontsize=18)
     plt.ylabel('CGM (mg/dL)', fontsize=18)
-    plt.legend(['Real', 'Predictions', "SMBG", "Artificial SMBG"], loc='lower right')
+    plt.legend(['Real', 'Predictions', "Real SMBG", "Artificial SMBG"], loc='lower right')
     plt.figtext(0.5, 0.01, f"Parameters: Batch Size: {batch_size}; #Epochs: {epochs}; Learning Rate: {learning_rate};"
                            f" Maximum SMGB Distance: {SMBG_window}h; Prediction Window: {round(prediction_window * 5 / 60, 2)}h",
                 ha='center')
@@ -382,7 +422,8 @@ if __name__ == "__main__":
     '''
     # Configuration flags
     use_ubelix = False  # Set to True to run on UBELIX, set False to run locally
-    model_type = 'intra'  # 'inter', 'intra', 'all'
+    model_type = 'intra'  # 'inter' to train model with all patients except the test one
+    # 'intra', to train and test model on only one patient
 
     # Hyperparameters:
     batch_size = 150
@@ -391,10 +432,11 @@ if __name__ == "__main__":
     prediction_window = 80
     SMBG_window = 4  # in hours
 
-    # patient_ids = {
-    #     '2018': [559, 563, 570, 575, 588, 591],
-    #     '2020': [540, 544, 552, 567, 584, 596]
-    # }
+    patient_id = 570    # Choose a patient ID from the list below
+
+    # List of patient excel IDs in the year folders
+    patient_ids_2018 = [559, 563, 570, 575, 588, 591]
+    patient_ids_2020 = [540, 544, 552, 567, 584, 596]
 
     # Load the dataset paths on Ubelix or Locally
     current_directory = os.getcwd()
@@ -402,10 +444,6 @@ if __name__ == "__main__":
         data_folder = os.path.join(current_directory, 'dataset')
     else:
         data_folder = os.path.join(os.path.dirname(current_directory), 'dataset')
-
-    # List of patient excel IDs in the year folders
-    patient_ids_2018 = [559, 563, 570, 575, 588, 591]
-    patient_ids_2020 = [540, 544, 552, 567, 584, 596]
 
     train_set_2018 = []
     test_set_2018 = []
@@ -454,23 +492,35 @@ if __name__ == "__main__":
     all_train_2020 = pd.concat(train_set_2020, ignore_index=True)
     all_test_2020 = pd.concat(test_set_2020, ignore_index=True)
 
-    # TODO: Improve the loading of data so we can test on 1 patient from 2018 or 2020, but also predict on all 12 patients
-    index_patient_570 = 2
-    train_set = train_set_2018[index_patient_570]
+    all_patient_ids = [559, 563, 570, 575, 588, 591, 540, 544, 552, 567, 584, 596]
 
-    test_set = test_set_2018[index_patient_570]
+    patient_index = None
 
-    # Select input data
+    for i, patient_number in enumerate(all_patient_ids):
+        if patient_number == patient_id:
+            if i < len(patient_ids_2018) - 1:
+                patient_index = i
+                data_from_2018 = True
+            else:
+                patient_index = i - len(patient_ids_2018)
+                data_from_2018 = False
+            break
+
+    if patient_index == None:
+        raise ValueError("The patient ID is not in the dataset.")
+
+    if data_from_2018:
+        test_set = test_set_2018[patient_index]
+    else:
+        test_set = test_set_2020[patient_index]
+
+    all_training_set = []
+
     train_input_data = None
     model_type_statement = None  # for the plotting of the results
 
-    # TODO: Improve the loading of data so we can test on 1 patient from 2018 or 2020, but also predict on all 12 patients
-    patient_index = 2
-    all_training_set = []
-    data_from_2018 = True  # True if the data is from 2018, False if the data is from 2020
-
     if model_type == 'inter':  # Concatenate the data of all patients excluding the patient under investigation
-        model_type_statement = "inter-patient model"
+        model_type_statement = "Inter-Patient Model"
         for i in range(len(patient_ids_2018)):
             if i != patient_index:
                 all_training_set.append(
@@ -484,18 +534,15 @@ if __name__ == "__main__":
                 else:
                     all_training_set.append(
                         train_set_2018[i])
-        all_training_set = pd.concat(all_training_set, ignore_index=True)
-        train_input_data = all_training_set
+        train_input_data = pd.concat(all_training_set, ignore_index=True)
     elif model_type == 'intra':
-        model_type_statement = "intra-patient model"
+        model_type_statement = "Intra-Patient Model"
         if data_from_2018:
-            train_input_data = train_set_2018[patient_index]  # only one patient
+            train_input_data = train_set_2018[patient_index]
         else:
             train_input_data = train_set_2020[patient_index]
-    elif model_type == 'all':
-        model_type_statement = "all-patient model"
-        all_training_set = pd.concat([all_train_2018, all_train_2020], ignore_index=True)
-        train_input_data = all_training_set
+    else:
+        raise ValueError("The model type is not valid, please use 'intra' or 'inter'.")
 
     # Read the data of the patient
     x_train, y_train, x_test, y_test, continuous_ytest, scalers_transforms_train, scalers_transforms_test, test_set_plotting, train_artificial_SMBG_indices, test_artificial_SMBG_indices = read_patient(
